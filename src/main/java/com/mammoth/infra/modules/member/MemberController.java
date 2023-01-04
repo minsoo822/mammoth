061ea1a13c21b81f1c1751c87d7eb6b01387d6d1
minsoo822@naver.com <minsoo822@naver.com>
2023-01-04 15:47:26 +0900

    컨트롤러 추가중

--- src/main/java/com/mammoth/infra/modules/member/MemberController.java
+++ src/main/java/com/mammoth/infra/modules/member/MemberController.java
@@ -1,5 +1,27 @@
 package com.mammoth.infra.modules.member;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/")
 public class MemberController {
 
+	@RequestMapping(value="login")
+	public String login() throws Exception {
+		return "infra/member/login";
+	}
+	
+	@RequestMapping(value="orderList")
+	public String orderList() throws Exception {
+		return "infra/member/orderList";
+	}
+	
+	@RequestMapping(value="basket")
+	public String basket() throws Exception {
+		return "infra/member/basket";
+	}
+	
+	
+	
 }

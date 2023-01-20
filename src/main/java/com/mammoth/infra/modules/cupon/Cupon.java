e29f979d1dba7e26adfccfb3afa35befccbc0c76
wjddnrud <dnwk203@naver.com>
2023-01-20 15:43:40 +0900

    cupon dto 추가

---
+++ src/main/java/com/mammoth/infra/modules/cupon/Cupon.java
@@ -0,0 +1,60 @@
+package com.mammoth.infra.modules.cupon;
+
+public class Cupon {
+
+	private Integer cpSeq;
+	private String cpName;
+	private Integer cpDiscount;
+	private Integer cpPercentNy;
+	private Integer cpUseNy;
+	private Integer cpDelNy;
+	private Integer cp_mmSeq;
+	
+	
+	
+	public Integer getCpSeq() {
+		return cpSeq;
+	}
+	public void setCpSeq(Integer cpSeq) {
+		this.cpSeq = cpSeq;
+	}
+	public String getCpName() {
+		return cpName;
+	}
+	public void setCpName(String cpName) {
+		this.cpName = cpName;
+	}
+	public Integer getCpDiscount() {
+		return cpDiscount;
+	}
+	public void setCpDiscount(Integer cpDiscount) {
+		this.cpDiscount = cpDiscount;
+	}
+	public Integer getCpPercentNy() {
+		return cpPercentNy;
+	}
+	public void setCpPercentNy(Integer cpPercentNy) {
+		this.cpPercentNy = cpPercentNy;
+	}
+	public Integer getCpUseNy() {
+		return cpUseNy;
+	}
+	public void setCpUseNy(Integer cpUseNy) {
+		this.cpUseNy = cpUseNy;
+	}
+	public Integer getCpDelNy() {
+		return cpDelNy;
+	}
+	public void setCpDelNy(Integer cpDelNy) {
+		this.cpDelNy = cpDelNy;
+	}
+	public Integer getCp_mmSeq() {
+		return cp_mmSeq;
+	}
+	public void setCp_mmSeq(Integer cp_mmSeq) {
+		this.cp_mmSeq = cp_mmSeq;
+	}
+	
+	
+	
+}
